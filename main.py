fd06e98048a87bda610f143f0ce2a08ab638dbfe
nukin836-code <nukin836@gmail.com>
2026-09-04 02:41:16 +0000

    пздц 1 символ 1 гит

--- main.py
+++ main.py
@@ -60,7 +60,7 @@ async def cancel_addition(message: Message, state: FSMContext) -> None:
     await message.answer("Добавление звука отменено.")
 
 from aiogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
-     from aiogram.fsm.context import FSMContext
+from aigram.fsm.context import FSMContext
 
 @dp.message(Command("start"), F.chat.type == "private")
 
